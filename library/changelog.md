664482df4499d24f42b4ff65011dfb6f63a23633
JiJi <admin@jifwq.com>
2020-08-04 10:47:06 +0800

    update

--- library/changelog.md
+++ library/changelog.md
@@ -1,14 +1,19 @@
-gpslib.h 更新日志 作者：吉吉
+# gpslib.h 更新日志
+作者：吉吉
 
-v1.1 2020年1月28日
-修复 char* getCode(double lat, double lon)的野内存会导致字符串溢出的bug
-兼容 51单片机 可直接包含本头文件
-新增 RMC结构体
-新增 解析RMC信息 RMC rmc2data(char* str)
-完善 度分经纬度格式转换 double degree2format(double value) 的介绍
-优化 double* gps2data(char* str)的算法
+## v1.2 2020年8月4日
+* 修复 `char* getCode(double lat, double lon)` 部分区域会计算错误
+* 优化 返回值由堆中动态分配
 
-v1.0 2020年1月9日
-新增 解析GPS芯片的经纬度 double* gps2data(char* str)
-新增 通过经纬度获取梅登黑德格网编号 char* getCode(double lat, double lon)
-注意 代码全部使用UTF-8编码 如有乱码请先转为ANSI
+## v1.1 2020年1月28日
+* 修复 `char* getCode(double lat, double lon)` 的野内存会导致字符串溢出的bug
+* 兼容 51单片机 可直接包含本头文件
+* 新增 RMC结构体
+* 新增 解析RMC信息 `RMC rmc2data(char* str)`
+* 完善 度分经纬度格式转换 `double degree2format(double value)` 的介绍
+* 优化 `double* gps2data(char* str)` 的算法
+
+## v1.0 2020年1月9日
+* 新增 解析GPS芯片的经纬度 `double* gps2data(char* str)`
+* 新增 通过经纬度获取梅登黑德格网编号 `char* getCode(double lat, double lon)`
+* 注意 代码全部使用 `UTF-8` 编码，如有乱码请转为 `ANSI`
--- library/gpslib.h
+++ library/gpslib.h
@@ -1,6 +1,6 @@
 /*
  * This file is storaged by UTF-8 encoding, if you cannot load it normally, try to transform it to ANSI.
- * GPSLIB.H v1.1
+ * GPSLIB.H v1.2
  * 作者: 吉吉(JiJi)
  * https://github.com/mmdjiji
  *
@@ -41,46 +41,50 @@ typedef struct RMC {
 	double direction;		//地面航向
 } RMC;
 double degree2format(double value) {
-	int negative = 0, du;
-	double fen;
+	int negative = 0, degree;
+	double minute;
 	if(value < 0) {
 		negative = 1;
 		value = -value;
 	}
-	du = floor(value / 100);
-	fen = value - du*100;
-	if(negative)du=-du;
-	return du + (fen/60);
+	degree = floor(value / 100);
+	minute = value - degree*100;
+	if(negative) degree = -degree;
+	return degree + (minute/60);
 }
 double* gps2data(char* str) {
-	static double ret[2];
+	double *retval = (double*)malloc(2*sizeof(double));
 	char *tokenPtr=strtok(str,",");
-	ret[0] = atof(tokenPtr);
+	retval[0] = atof(tokenPtr);
 	tokenPtr=strtok(NULL,",");
-	if(tokenPtr[0] == 'S')ret[0] = -ret[0];
+	if(tokenPtr[0] == 'S')retval[0] = -retval[0];
 	tokenPtr=strtok(NULL,",");
-	ret[1] = atof(tokenPtr);
+	retval[1] = atof(tokenPtr);
 	tokenPtr=strtok(NULL,",");
-	if(tokenPtr[0] == 'W')ret[1] = -ret[1];
-	ret[0] = degree2format(ret[0]);
-	ret[1] = degree2format(ret[1]);
-	return ret;
+	if(tokenPtr[0] == 'W')retval[1] = -retval[1];
+	retval[0] = degree2format(retval[0]);
+	retval[1] = degree2format(retval[1]);
+	return retval;
 }
 char* getCode(double lat, double lon) {
-	static char ucode[7];
-	if(lon<0)lon+=360;
-	if(lat<0)lat+=180;
-	ucode[0] = (char)((int)((180 + lon) / 20)+(int)'A');
-	ucode[1] = (char)((int)((90 + lat) / 10)+(int)'A');
-	ucode[2] = (char)((int)((int)(lon + 180) % 20 / 2)+(int)'0');
-	ucode[3] = (char)((int)(lat + 90) % 10+(int)'0');
-	ucode[4] = (char)((int)((lon - (int)(lon / 2) * 2) * 60 / 5)+(int)'A');
-	ucode[5] = (char)((int)((lat - (int)(lat)) * 60 / 2.5)+(int)'A');
-	ucode[6] = '\0';
-	return ucode;
+	char *retval = (char*)malloc(7*sizeof(char));
+	while(lon < -180) lon += 360;
+	while(lon > +180) lon -= 360;
+	while(lat < -90) lat += 180;
+	while(lat > +90) lat -= 180;
+	retval[0] = (char)((int)((180 + lon) / 20)+(int)'A');
+	retval[1] = (char)((int)((90 + lat) / 10)+(int)'A');
+	retval[2] = (char)((int)((int)(lon + 180) % 20 / 2)+(int)'0');
+	retval[3] = (char)((int)(lat + 90) % 10+(int)'0');
+	while(lon < 0) lon += 180;
+	while(lat < 0) lat += 90;
+	retval[4] = (char)((int)((lon - (int)(lon / 2) * 2) * 60 / 5)+(int)'A');
+	retval[5] = (char)((int)((lat - (int)(lat)) * 60 / 2.5)+(int)'A');
+	retval[6] = '\0';
+	return retval;
 }
 RMC rmc2data(char* str) {
-	static RMC output;
+	RMC retval;
 	char buff[20] = {0};
 	int i, j, count = 0, buffs = 0;
 	for(i=0; i<strlen(str); i++) {
@@ -92,38 +96,37 @@ RMC rmc2data(char* str) {
 			count = 0;
 			switch(buffs++) {
 				case 1:
-					for(j=0; j<6; j++) output.UTC_TIME[j] = buff[j];
+					for(j=0; j<6; j++) retval.UTC_TIME[j] = buff[j];
 					break;
 				case 2:
-					output.valid = buff[0];
+					retval.valid = buff[0];
 					break;
 				case 3:
-					output.lat = degree2format(atof(buff));
+					retval.lat = degree2format(atof(buff));
 					break;
 				case 4:
-					if(buff[0] == 'S') output.lat = -output.lat;
+					if(buff[0] == 'S') retval.lat = -retval.lat;
 					break;
 				case 5:
-					output.lon = degree2format(atof(buff));
+					retval.lon = degree2format(atof(buff));
 					break;
 				case 6:
-					if(buff[0] == 'W') output.lon = -output.lon;
+					if(buff[0] == 'W') retval.lon = -retval.lon;
 					break;
 				case 7:
-					output.speed = atof(buff);
+					retval.speed = atof(buff);
 					break;
 				case 8:
-					output.direction = atof(buff);
+					retval.direction = atof(buff);
 					break;
 				case 9:
-					for(j=0; j<6; j++) output.UTC_DATE[j] = buff[j];
+					for(j=0; j<6; j++) retval.UTC_DATE[j] = buff[j];
 					break;
 			}
-
 		}
 	}
-	output.UTC_TIME[6] = '\0';
-	output.UTC_DATE[6] = '\0';
-	return output;
+	retval.UTC_TIME[6] = '\0';
+	retval.UTC_DATE[6] = '\0';
+	return retval;
 }
 #endif
